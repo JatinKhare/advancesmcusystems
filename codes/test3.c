e5995727810790b5f90fb581aecfd0205e4405d9
Jatin Khare <kharejatin1@gmail.com>
2022-02-13 10:16:34 -0600

    minor changed done

--- codes/test3.c
+++ codes/test3.c
@@ -23,6 +23,7 @@
 #include <sys/mman.h>
 #include <sys/time.h>
 #include <time.h>
+#include <signal.h>
 
 #define CDMA                0xB0000000
 #define BRAM                0xB0028000
@@ -104,80 +105,212 @@ void transfer_back(unsigned int *cdma_virtual_address, int length){
     cdma_sync(cdma_virtual_address);
 }
 
-int main() {
+uint32_t* cdma_virtual_address;
+uint32_t* BRAM_virtual_address;
+void m_unmap_ctrl_c(int sig_num){
+
+        munmap(cdma_virtual_address, MAP_SIZE);
+        munmap(BRAM_virtual_address, MAP_SIZE);
+	printf("\n\nmunmap() done!\nNow terminating the process with grace...\n\n");
+	kill(0,SIGKILL);
+}
+int main(int argc, char *argv[]) {
+
+    signal(SIGINT, m_unmap_ctrl_c);
     int dh = open("/dev/mem", O_RDWR | O_SYNC); // Open /dev/mem which represents the whole physical memory
-    uint32_t* cdma_virtual_address = mmap(NULL, 
-                                          MAP_SIZE, 
-                                          PROT_READ | PROT_WRITE, 
-                                          MAP_SHARED, 
-                                          dh, 
-                                          CDMA & ~MAP_MASK); // Memory map AXI Lite register block
-    uint32_t* BRAM_virtual_address = mmap(NULL, 
-                                          MAP_SIZE, 
-                                          PROT_READ | PROT_WRITE, 
-                                          MAP_SHARED, 
-                                          dh, 
-                                          BRAM & ~MAP_MASK); // Memory map AXI Lite register block
-    int yy = 1024;
-    uint32_t data[yy];
+    
+    if(dh == -1){
+	    printf("Unable to open /dev/mem. Ensure if it exists.\n");
+	    return -1;
+    }
+
+    if(argc!=3 && argc!=1){
+	    printf("USAGE: ./test1 (xx loops) (yy words), and pass 0 to run with default values\n");
+	    return -1;
+    }
+
+
+    int xx = 0;   //default value of number of loops to run
+    int yy = 1024;   //default value of number of words to test per loop
+    int sw = 1;
+    if(argc == 2){
+	    xx = strtoul(argv[1], 0, 0);   //taking number of words from the user
+            printf("Changing the words/loop to yy = %d \n\n", yy);
+    }
+    if(argc == 3){ 
+	    xx = strtoul(argv[1], 0, 0);   //taking number of words from the user
+	    yy = strtoul(argv[2], 0, 0);   //taking number of loops from the user
+            printf("Changing the number of loops to xx = %d\n", xx);
+            printf("Changing the words/loop to yy = %d\n\n", yy);
+	    if(yy == 0)
+		    yy = 1024;
+    }
+    if(xx == 0)
+	    sw = 0;
+
+    
+
+    //Generating random data and address
+    int count = 0;
+    int LOOPS = xx;
     srand(time(0));
-    for(int i = 0; i < yy; i++){
-	    data[i] = rand();
+    switch(sw){
+	    case 1:
+		    cdma_virtual_address = mmap(NULL, 
+							  MAP_SIZE, 
+							  PROT_READ | PROT_WRITE, 
+							  MAP_SHARED, 
+							  dh, 
+							  CDMA & ~MAP_MASK); // Memory map AXI Lite register block
+		    BRAM_virtual_address = mmap(NULL, 
+							  MAP_SIZE, 
+							  PROT_READ | PROT_WRITE, 
+							  MAP_SHARED, 
+							  dh, 
+							  BRAM & ~MAP_MASK); // Memory map AXI Lite register block
+	    while(xx){
+		    uint32_t data[yy];
+		    srand(time(0));
+		    for(int i = 0; i < yy; i++){
+			    data[i] = rand();
+		    }
+
+		    uint32_t* ocm_1 = mmap(NULL, 
+					   OCM_MAP_SIZE, 
+					   PROT_READ | PROT_WRITE, 
+					   MAP_SHARED, 
+					   dh, 
+					   OCM_1);
+
+		    uint32_t* ocm_2 = mmap(NULL, 
+					   OCM_MAP_SIZE, 
+					   PROT_READ | PROT_WRITE, 
+					   MAP_SHARED, 
+					   dh, 
+					   OCM_2);
+
+		    for(int i=0; i<yy; i++)
+			ocm_1[i] = data[i];
+		    
+		    // RESET DMA
+		    dma_set(cdma_virtual_address, CDMACR, 0x04);
+		   
+		    //struct timeval start, end;
+		    //printf("Source memory block:      "); memdump(cdma_virtual_address, yy);
+		    //printf("Destination memory block: "); memdump(BRAM_virtual_address, yy);
+
+		    transfer(cdma_virtual_address, yy);
+		    printf("OCM to BRAM: Transfer of %d words successful!\n\n", yy);
+
+		    transfer_back(cdma_virtual_address, yy);
+		    printf("BRAM to OCM: Transfer of %d words successful!\n\n", yy);
+		 
+		    //for(int i = 0; i<yy; i++){
+		    //	    printf("ocm1[%d] = %d, ocm2[%d] = %d\n", i, ocm_1[i], i, ocm_2[i]);
+		    //}
+		    
+		    for(int i=0; i<yy; i++)
+		    {
+			if(ocm_2[i] != ocm_1[i])
+			{
+			    printf("\nTest failed!!\n");
+			    printf("OCM2 result: %d and OCM1 result is %d for the element %d\n", ocm_2[i], ocm_1[i], i+1);
+			    munmap(ocm_1, OCM_MAP_SIZE);
+			    munmap(ocm_2, OCM_MAP_SIZE);
+			    munmap(cdma_virtual_address, MAP_SIZE);
+			    munmap(BRAM_virtual_address,MAP_SIZE);
+			    return -1;
+			}
+		    }
+		   
+		    printf("\nDMA's OCM/BRAM traffic test loop %d with %d words successful!!!\n", LOOPS - xx + 1, yy);
+		    munmap(ocm_1, OCM_MAP_SIZE);
+		    munmap(ocm_2, OCM_MAP_SIZE);
+		    xx--;
+		   
+	    }
+		    munmap(cdma_virtual_address, MAP_SIZE);
+		    munmap(BRAM_virtual_address,MAP_SIZE);
+		    break;
+
+	    case 0:
+	    while(1){
+		    cdma_virtual_address = mmap(NULL, 
+							  MAP_SIZE, 
+							  PROT_READ | PROT_WRITE, 
+							  MAP_SHARED, 
+							  dh, 
+							  CDMA & ~MAP_MASK); // Memory map AXI Lite register block
+		    BRAM_virtual_address = mmap(NULL, 
+							  MAP_SIZE, 
+							  PROT_READ | PROT_WRITE, 
+							  MAP_SHARED, 
+							  dh, 
+							  BRAM & ~MAP_MASK); // Memory map AXI Lite register block
+		    uint32_t data[yy];
+		    srand(time(0));
+		    for(int i = 0; i < yy; i++){
+			    data[i] = rand();
+		    }
+
+		    uint32_t* ocm_1 = mmap(NULL, 
+					   OCM_MAP_SIZE, 
+					   PROT_READ | PROT_WRITE, 
+					   MAP_SHARED, 
+					   dh, 
+					   OCM_1);
+
+		    uint32_t* ocm_2 = mmap(NULL, 
+					   OCM_MAP_SIZE, 
+					   PROT_READ | PROT_WRITE, 
+					   MAP_SHARED, 
+					   dh, 
+					   OCM_2);
+
+		    for(int i=0; i<yy; i++)
+			ocm_1[i] = data[i];
+		    
+		    // RESET DMA
+		    dma_set(cdma_virtual_address, CDMACR, 0x04);
+		   
+		    //struct timeval start, end;
+		    //printf("Source memory block:      "); memdump(cdma_virtual_address, yy);
+		    //printf("Destination memory block: "); memdump(BRAM_virtual_address, yy);
+
+		    transfer(cdma_virtual_address, yy);
+		    printf("OCM to BRAM: Transfer of %d words successful!\n\n", yy);
+
+		    transfer_back(cdma_virtual_address, yy);
+		    printf("BRAM to OCM: Transfer of %d words successful!\n\n", yy);
+		 
+		    //for(int i = 0; i<yy; i++){
+		    //	    printf("ocm1[%d] = %d, ocm2[%d] = %d\n", i, ocm_1[i], i, ocm_2[i]);
+		    //}
+		    
+		    for(int i=0; i<yy; i++)
+		    {
+			if(ocm_2[i] != ocm_1[i])
+			{
+			    printf("\nTest failed!!\n");
+			    printf("OCM2 result: %d and OCM1 result is %d for the element %d\n", ocm_2[i], ocm_1[i], i+1);
+			    munmap(ocm_1, OCM_MAP_SIZE);
+			    munmap(ocm_2, OCM_MAP_SIZE);
+			    munmap(cdma_virtual_address, MAP_SIZE);
+			    munmap(BRAM_virtual_address,MAP_SIZE);
+			    return -1;
+			}
+		    }
+		    count++;
+		    printf("\nDMA's OCM/BRAM traffic test loop %d with %d words successful!!!\n", count, yy);
+		    munmap(ocm_1, OCM_MAP_SIZE);
+		    munmap(ocm_2, OCM_MAP_SIZE);
+		    munmap(cdma_virtual_address, MAP_SIZE);
+		    munmap(BRAM_virtual_address,MAP_SIZE);
+		   
+	    }
+		    break;
+
     }
 
-    uint32_t* ocm_1 = mmap(NULL, 
-		           OCM_MAP_SIZE, 
-			   PROT_READ | PROT_WRITE, 
-			   MAP_SHARED, 
-			   dh, 
-			   OCM_1);
-
-    uint32_t* ocm_2 = mmap(NULL, 
-		           OCM_MAP_SIZE, 
-			   PROT_READ | PROT_WRITE, 
-			   MAP_SHARED, 
-			   dh, 
-			   OCM_2);
-
-    for(int i=0; i<yy; i++)
-        ocm_1[i] = data[i];
-    
-    // RESET DMA
-    dma_set(cdma_virtual_address, CDMACR, 0x04);
-   
-    //struct timeval start, end;
-    //printf("Source memory block:      "); memdump(cdma_virtual_address, yy);
-    //printf("Destination memory block: "); memdump(BRAM_virtual_address, yy);
-
-    transfer(cdma_virtual_address, yy);
-    printf("OCM to BRAM: Transfer of %d words successful!\n\n", yy);
-
-    transfer_back(cdma_virtual_address, yy);
-    printf("BRAM to OCM: Transfer of %d words successful!\n\n", yy);
- 
-    //for(int i = 0; i<yy; i++){
-    //	    printf("ocm1[%d] = %d, ocm2[%d] = %d\n", i, ocm_1[i], i, ocm_2[i]);
-    //}
-    
-    for(int i=0; i<yy; i++)
-    {
-        if(ocm_2[i] != ocm_1[i])
-        {
-            printf("\nTest failed!!\n");
-            printf("OCM2 result: %d and OCM1 result is %d for the element %d\n", ocm_2[i], ocm_1[i], i+1);
-            munmap(ocm_1, OCM_MAP_SIZE);
-            munmap(ocm_2, OCM_MAP_SIZE);
-            munmap(cdma_virtual_address, MAP_SIZE);
-            munmap(BRAM_virtual_address,MAP_SIZE);
-            return -1;
-        }
-    }
-   
-    printf("\nDMA's OCM/BRAM traffic test with %d words successful!!!\n", yy);
-    munmap(ocm_1, OCM_MAP_SIZE);
-    munmap(ocm_2, OCM_MAP_SIZE);
-    munmap(cdma_virtual_address, MAP_SIZE);
-    munmap(BRAM_virtual_address,MAP_SIZE);
-   
     return 0;
 }
