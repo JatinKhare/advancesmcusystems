b787dd0688b39cd86a92dcff78c0d51786fc9778
Jatin Khare <kharejatin1@gmail.com>
2022-03-04 17:29:04 -0600

    removed the temporary files

---
+++ lab2/delete/kernal_modules/km_insert.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+make clean
+make
+
+rm /dev/cdma_int
+mknod /dev/cdma_int c 241 0
+
+#rmmod cdma_int.ko
+insmod cdma_int.ko
+
+rm /dev/captimer_int
+mknod /dev/captimer_int c 240 0
+
+#rmmod captimer_int.ko
+insmod captimer_int.ko
+
